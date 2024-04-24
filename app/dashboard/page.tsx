33aa24b9f52ae46e76db49baeed09c256b59eaf8
Jude Okorie <jude58005@gmail.com>
2024-04-24 10:25:08 +0100

    removed mantine configs #2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import { auth } from '@/auth'
 import { redirect } from 'next/navigation'
 import LogoutButton from '@/components/ui/logout'
-import { Pill } from '@mantine/core'
 
 export default async function Dashboard() {
   const session = await auth()
@@ -10,10 +9,8 @@ export default async function Dashboard() {
   }
   return (
     <>
-      {/* <Alert color="warning">Alert!</Alert> */}
+      <h1>Protected Page</h1>
 
-      {/* <h1>Protected Page</h1>
-      <Pill c="red">Working???</Pill> */}
       <pre>Welcome {JSON.stringify(session, null, 2)}!</pre>
       <LogoutButton />
     </>
