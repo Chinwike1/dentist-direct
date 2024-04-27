55a9b2507adcbcd7460cd5419086d4452bec7bbd
Chinwike <maduabuchichizzy@gmail.com>
2024-04-27 11:22:19 +0100

    chore: edit hero copy

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -6,37 +6,27 @@ import { usePathname } from 'next/navigation'
 
 type NavLinks = { name: string; path: string }
 
-const navLinks: NavLinks[] = [
-  {
-    name: 'About Us',
-    path: '/',
-  },
-  {
-    name: 'Pricing',
-    path: '/',
-  },
-]
+// const navLinks: NavLinks[] = [
+//   {
+//     name: 'About Us',
+//     path: '/',
+//   },
+//   {
+//     name: 'Pricing',
+//     path: '/',
+//   },
+// ]
 
 export default function Navbar() {
   const pathname = usePathname()
 
   return (
     <header className="fixed left-0 top-0 flex h-[--nav-height] w-full items-center bg-white px-7 shadow-md">
-      <Link href="/" className="">
+      <Link href="/" className="flex items-center gap-3">
         <Logo />
+        <span className="font-bold text-aqua">Dentist Direct</span>
       </Link>
-      <ul className="mx-auto ml-10 hidden gap-8 text-slate-900 lg:flex">
-        {navLinks.map((link) => (
-          <Link
-            key={link.name}
-            className="font-light transition-colors hover:underline"
-            href={link.path}
-          >
-            {link.name}
-          </Link>
-        ))}
-      </ul>
-      <div className="border- ml-auto flex gap-2">
+      <div className="ml-auto flex gap-2">
         <Link
           href="/dashboard?action=book"
           className="ml-auto hidden items-center rounded-full px-3 py-2 transition-colors hover:underline md:flex"
@@ -47,14 +37,14 @@ export default function Navbar() {
         {pathname === '/login' ? (
           <Link
             href="/register"
-            className="border-aqua text-aqua hover:bg-aqua hover:text-aqua-100 ml-auto flex items-center gap-2 rounded-2xl border-[1.5px] px-3 py-2 font-semibold transition-colors"
+            className="ml-auto flex items-center gap-2 rounded-2xl border-[1.5px] border-aqua px-3 py-2 font-semibold text-aqua transition-colors hover:bg-aqua hover:text-aqua-100"
           >
             Register Now <ChevronRightIcon className="h-5 w-5" />
           </Link>
         ) : (
           <Link
             href="/login"
-            className="border-aqua text-aqua hover:bg-aqua hover:text-aqua-100 ml-auto flex items-center gap-2 rounded-2xl border-[1.5px] px-3 py-2 font-semibold transition-colors"
+            className="ml-auto flex items-center gap-2 rounded-2xl border-[1.5px] border-aqua px-3 py-2 font-semibold text-aqua transition-colors hover:bg-aqua hover:text-aqua-100"
           >
             Sign In <ChevronRightIcon className="h-5 w-5" />
           </Link>
